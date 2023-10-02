fbbd6823cf65ac86f0f1b21f665474526281f36a
Shubham_tal <shubham.garg@theapplabb.com>
2023-10-02 16:26:03 +0530

    Code Refactor.

--- app/src/main/java/com/example/digimantra/data/BeerApi.kt
+++ app/src/main/java/com/example/digimantra/data/BeerApi.kt
@@ -6,7 +6,7 @@ import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface BeerApi {
-    @GET("v2/beers")
+    @GET("beers")
     suspend fun getBeersList(
         @Query("page") page: Int,
         @Query("per_page") limit: Int
--- app/src/main/java/com/example/digimantra/utils/AppConstants.kt
+++ app/src/main/java/com/example/digimantra/utils/AppConstants.kt
@@ -2,7 +2,7 @@ package com.example.digimantra.utils
 
 object AppConstants {
 
-    const val BASE_URL = "https://api.punkapi.com"
+    const val BASE_URL = "https://api.punkapi.com/v2/"
     const val PAGE_SIZE = 10
 
 }
